4b4278ee07f2e9aef85cc575745fed51940a7922
Aleksei Zhuvikin <zhuvikin@gmail.com>
2022-03-10 22:48:41 -0800

    Test watermarking of 256 x 256 size image

--- watermarking/src/test/java/ru/zhuvikin/auth/watermarking/SelectiveImageAuthenticationTest.java
+++ watermarking/src/test/java/ru/zhuvikin/auth/watermarking/SelectiveImageAuthenticationTest.java
@@ -16,20 +16,26 @@ public class SelectiveImageAuthenticationTest {
 
     private static final String PASSWORD = "password";
     private static final int SIGNATURE_LENGTH = 1024;
+    private static final int SIGNATURE_LENGTH_2 = 512;
 
     private static final RsaKeys RSA_KEYS = new RsaKeys(PASSWORD, SIGNATURE_LENGTH);
+    private static final RsaKeys RSA_KEYS_512 = new RsaKeys(PASSWORD, SIGNATURE_LENGTH_2);
 
     private static final String ORIGINAL_NAME = "lena.jpg";
+    private static final String ORIGINAL_NAME_256 = "lena_256.jpg";
     private static final String WATERMARKED_NAME_1 = "lena_watermarked_1.jpg";
     private static final String WATERMARKED_NAME_2 = "lena_watermarked_2.jpg";
     private static final String WATERMARKED_NAME_3 = "lena_watermarked_3.jpg";
+    private static final String WATERMARKED_NAME_256 = "lena_watermarked_256.jpg";
 
     private static final ClassLoader CLASS_LOADER = SelectiveImageAuthenticationTest.class.getClassLoader();
 
     private static final URL LENA_URL = CLASS_LOADER.getResource(ORIGINAL_NAME);
+    private static final URL LENA_256_URL = CLASS_LOADER.getResource(ORIGINAL_NAME_256);
     private static final URL LENA_WM1_URL = CLASS_LOADER.getResource(WATERMARKED_NAME_1);
     private static final URL LENA_WM2_URL = CLASS_LOADER.getResource(WATERMARKED_NAME_2);
     private static final URL LENA_WM3_URL = CLASS_LOADER.getResource(WATERMARKED_NAME_3);
+    private static final URL LENA_WM256_URL = CLASS_LOADER.getResource(WATERMARKED_NAME_256);
 
     private static final WatermarkingParameters WATERMARKING_PARAMETERS_1 = WatermarkingParameters.builder()
             .eccCodeRate(0.5)
@@ -108,6 +114,28 @@ public class SelectiveImageAuthenticationTest {
         BufferedImage image = ImageIO.read(LENA_WM3_URL);
 
         boolean authentic = SelectiveImageAuthentication.authenticate(image, WATERMARKING_PARAMETERS_3, RSA_KEYS.getPublicKey());
+
+        assertTrue(authentic);
+    }
+
+    @Test
+    @SneakyThrows
+    public void testWatermark4() {
+        BufferedImage image = ImageIO.read(LENA_256_URL);
+
+        BufferedImage watermarked = SelectiveImageAuthentication
+                .watermark(image, WATERMARKING_PARAMETERS_1, RSA_KEYS_512.getPrivateKey());
+
+        File outputFile = new File(WATERMARKED_NAME_256);
+        saveJPEG(watermarked, outputFile, 1f);
+    }
+
+    @Test
+    @SneakyThrows
+    public void testAuthenticate4() {
+        BufferedImage image = ImageIO.read(LENA_WM256_URL);
+
+        boolean authentic = SelectiveImageAuthentication.authenticate(image, WATERMARKING_PARAMETERS_1, RSA_KEYS_512.getPublicKey());
 
         assertTrue(authentic);
     }
